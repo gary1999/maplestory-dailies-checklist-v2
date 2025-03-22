fd0a99a95cdf7d4f82a4747aec6b0b93665aeb21
gary1999 <shengary1999@gmail.com>
2025-03-23 10:03:48 +1300

    Fix 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./assets/css/Navbar.css";
 import Clock from "./assets/components/Clock";
